9e6983c0ea70f9bc58182c1a601eed7801e7df9a
mostarymithi <mostarymithi@gmail.com>
2021-01-29 19:47:39 +0600

    30

---
+++ Into_to_tutorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int v,n,ar[1000],i;
+    scanf("%d",&v);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&ar[i]);
+    for(i=0;i<n;i++)
+        {
+        if(ar[i]==v)
+        printf("%d",i);
+    }
+
+    
+    return 0;
+}
